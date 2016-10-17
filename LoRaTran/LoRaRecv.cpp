dcfbe4f5d8abb3db3cc0f286314c58bbfb16779d
s91503486f <s91503486f@yahoo.com.tw>
2016-10-18 02:55:04 +0800

    增加rssi 收集與轉locateinfo過程

--- LoRaTran/LoRaRecv.cpp
+++ LoRaTran/LoRaRecv.cpp
@@ -181,6 +181,31 @@ int main (int argc, char **argv){
 			printf("recving data error!End Program");
 			return 1;
 		}
+		
+		bufferManger.buf_push(new_rssi);
+
+		rssi_arr = bufferManger.collectTripleRssi();
+
+		//若有收集到三個rssi  則包成locate_info並繼續做下去
+		if (rssi_arr != NULL) {
+			float d_arr[3];
+			for (int i = 0; i < 3; i++) {
+				d_arr[i] = Rssi_to_distance(*(rssi_arr + i), -50, 2);
+			}
+			
+			loca_info = FillIndoorLocate_info(d_arr[0], d_arr[1], d_arr[2]);
+
+		}
+		else {
+			continue;
+		}
+		
+					
+			
+		/*
+			將loca_info丟給distance_to_point計算;
+		*/
+		
 		count++;
 	}
 	
