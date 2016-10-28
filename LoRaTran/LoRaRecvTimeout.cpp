1487d1d5d583f2e1d4c5111f0de5867c2dd2d6cb
s91503486f <s91503486f@yahoo.com.tw>
2016-10-29 02:04:11 +0800

    optimize code

--- LoRaTran/LoRaRecvTimeout.cpp
+++ LoRaTran/LoRaRecvTimeout.cpp
@@ -119,88 +119,85 @@ Rssi_info* RecvRssi(){
 	return rssi_info;
 }
 
+void RecvWithTimeout(clock_t Timeout,clock_t Timeout_Begin,Rssi_info* A_buffer, Rssi_info*B_buffer, Rssi_info*C_buffer) {
+	clock_t Timeout_End;
 
-int main (int argc, char **argv){
-    float distance[3];
-    char output[50];
-    Rssi_info new_rssi;
-    Rssi_info packet[3];
-    Locate_info loca_info;
-    Point finalPoint;
-    Point BSpointA,BSpointB,BSpointC;
-    BSpointA=set_Bspoint(4.0,4.0, 'A');
-    BSpointB=set_Bspoint(9.0,7.0, 'B');
-    BSpointC=set_Bspoint(9.0,1.0, 'C');
-    Rssi_info* A_buffer = NULL ,*B_buffer = NULL,*C_buffer = NULL, *temp_rssi = NULL;
-	clock_t Timeout_Begin, Timeout_End;
+	do {
+		Rssi_info* temp_rssi = RecvRssi();
+		if (temp_rssi == NULL)continue;
+
+		switch (temp_rssi->deviceID[0]) {
+		case 'A':
+			if (A_buffer == NULL) {
+				A_buffer = temp_rssi;
+			}
+			break;
+		case 'B':
+			if (B_buffer == NULL) {
+				B_buffer = temp_rssi;
+			}
+			break;
+		case 'C':
+			if (C_buffer == NULL) {
+				C_buffer = temp_rssi;
+			}
+			break;
+		default:
+			break;
+		}
+		Timeout_End = clock();
+	} while (Timeout_End - Timeout_Begin < 10000 && !A_buffer && !B_buffer && !C_buffer);
+
+}
+
+void Rssi_to_point(Rssi_info* A_buffer, Rssi_info* B_buffer, Rssi_info* C_buffer) {
+	Locate_info loca_info;
+	Point finalPoint;
+	Point BSpointA, BSpointB, BSpointC;
+	BSpointA = set_Bspoint(4.0, 4.0, 'A');
+	BSpointB = set_Bspoint(9.0, 7.0, 'B');
+	BSpointC = set_Bspoint(9.0, 1.0, 'C');
+	char output[50];
+
+	if (!A_buffer || !B_buffer || !C_buffer) {}
+	else {
+		loca_info.distances[0] = Rssi_to_distance(*A_buffer, RSSI_A, RSSI_n);
+		printf("Point %c , Distance : %.2f \n", A_buffer->deviceID, loca_info.distances[0]);
+		loca_info.distances[1] = Rssi_to_distance(*B_buffer, RSSI_A, RSSI_n);
+		printf("Point %c , Distance : %.2f \n", B_buffer->deviceID, loca_info.distances[1]);
+		loca_info.distances[2] = Rssi_to_distance(*C_buffer, RSSI_A, RSSI_n);
+		printf("Point %c , Distance : %.2f \n", C_buffer->deviceID, loca_info.distances[2]);
+
+
+		finalPoint = trilateration(BSpointA, BSpointB, BSpointC, loca_info.distances[0], loca_info.distances[1], loca_info.distances[2]);
+		printf("finalPoint>> X: %.2f ,Y: %.2f \n", finalPoint.x, finalPoint.y);
+		sprintf(output, "finalPoint>> X: %.2f ,Y: %.2f \n", finalPoint.x, finalPoint.y);
+		fileInput(output);
+	}
+}
+
+
+int main (int argc, char **argv){   
+    Rssi_info* A_buffer = NULL ,*B_buffer = NULL,*C_buffer = NULL;
+	clock_t Timeout_Begin;
 	
 	//file Operation
 	if (argv[1] != NULL) {
 		fileOpen(argv[1]);
 	}
 
-    int count = 0;
     setup();
     printf("Begin receiving message ! \n");
 	//接收RSSI
-	while(true){
-		//新循環
+	while(true){//新循環
 		Timeout_Begin = clock();
 		A_buffer = NULL;
 		B_buffer = NULL;
 		C_buffer = NULL;
 		
 		//10秒內判斷是否收到
-		do{ 
-			temp_rssi = RecvRssi();
-			
-			switch (temp_rssi->deviceID[0]) {
-				case 'A':
-					if(A_buffer == NULL){
-						A_buffer = temp_rssi;
-					}
-					else{
-						//nothing
-					}
-					break;
-				case 'B':
-					if(B_buffer == NULL){
-						B_buffer = temp_rssi;
-					}
-					else{
-						//nothing
-					}
-					break;
-				case 'C':
-					if(C_buffer == NULL){
-						C_buffer = temp_rssi;
-					}
-					else{
-						//nothing
-					}
-					break;
-				default:
-					break;
-			}
-			Timeout_End = clock();
-		} while (Timeout_End - Timeout_Begin < 10000 && !A_buffer && !B_buffer && !C_buffer);
-
-
-		if(!A_buffer||!B_buffer||!C_buffer){}
-		else{
-			loca_info.distances[0] = Rssi_to_distance(*A_buffer, RSSI_A, RSSI_n);
-			printf("Point %c , Distance : %.2f \n",A_buffer->deviceID,loca_info.distances[0]);
-			loca_info.distances[1] = Rssi_to_distance(*B_buffer, RSSI_A, RSSI_n);
-			printf("Point %c , Distance : %.2f \n",B_buffer->deviceID,loca_info.distances[1]);
-			loca_info.distances[2] = Rssi_to_distance(*C_buffer, RSSI_A, RSSI_n);
-			printf("Point %c , Distance : %.2f \n",C_buffer->deviceID,loca_info.distances[2]);
-		
-		
-			finalPoint=trilateration(BSpointA, BSpointB, BSpointC, loca_info.distances[0],loca_info.distances[1],loca_info.distances[2]);
-			printf("finalPoint>> X: %.2f ,Y: %.2f \n",finalPoint.x,finalPoint.y);
-			sprintf(output,"finalPoint>> X: %.2f ,Y: %.2f \n",finalPoint.x,finalPoint.y);
-			fileInput(output);
-		}
+		RecvWithTimeout(10 * 1000, Timeout_Begin, A_buffer, B_buffer, C_buffer);
+		Rssi_to_point(A_buffer, B_buffer, C_buffer);
 	}
     return (0);
 }
