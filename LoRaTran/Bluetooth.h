43837ef491c7a8d5b41cf745d256c04349abb042
sandy89876 <huyuxuan@huyuxuandeMacBook-Pro.local>
2016-11-10 20:51:31 +0800

    藍牙正確版

--- LoRaTran/Bluetooth.h
+++ LoRaTran/Bluetooth.h
@@ -16,20 +16,42 @@ using namespace std;
 #ifndef bluetooth_h
 #define bluetooth_h
 bdaddr_t my_bdaddr_any = {0,0,0,0,0,0};
-int s,client, bytes_read;
+int s,client, bytes_read=0;
 struct sockaddr_rc loc_addr = {0},rem_addr={0};
+char mes[20]={"0"};
 
 struct position{
-    char *latitude;
-    char *longitude;
+    char latitude[10];
+    char longitude[10];
 };
 typedef struct position Position;
+
+int findLab(char tmp){
+	for(int i=1;i<=sizeof(mes);i++){
+		if(mes[i]==tmp){
+			//cout<<"find "<<tmp<<" at "<<i<<endl;
+			return i;
+		}
+	}
+	cout<<"cannot find "<<tmp<<endl;
+}
+
+void copyToString(char *str,int start,int end){
+	int j=0;	
+	for(int i=start;i<=end;i++){
+		str[j]=mes[i];
+		j++;
+	}
+	str[j]=0;
+	//cout<<"after copy:str = "<<str;
+}
+
 void BluetoothInitial(){
     socklen_t opt = sizeof(rem_addr);
     s = socket(AF_BLUETOOTH,SOCK_STREAM,BTPROTO_RFCOMM);
     
     loc_addr.rc_family = AF_BLUETOOTH;
-    loc_addr.rc_bdaddr = *BDADDR_ANY;//any local Bluetooth adapter is acceptable
+    loc_addr.rc_bdaddr = my_bdaddr_any;//any local Bluetooth adapter is acceptable
     loc_addr.rc_channel = (uint8_t) 22; //port number to listen
     bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
     cout<<"bind on 22 complete"<<endl;
@@ -37,6 +59,7 @@ void BluetoothInitial(){
     cout<<"listen on s 1"<<endl;
     
     client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+	cout<<"client: "<<client<<endl;
     char buf[1024]={0};
     ba2str( &rem_addr.rc_bdaddr, buf );
     cout<<"accepted connection from"<<buf<<endl;
@@ -44,30 +67,35 @@ void BluetoothInitial(){
     memset(buf, 0, sizeof(buf));
 }
 
-Position readFromBluetooth(){
-    char buf[1024]={0};
+Position readFromBluetooth(){    
     Position local;
+	
     // read data from the client
-    bytes_read =unistd:read(client, buf, sizeof(buf));
-    
-    if( bytes_read > 0 ) {
-        std::size_t found = buf.find("%");
-        std::size_t length = buf.copy(local.latitude,found-1,1);
-        local.latitude[length]="\0";
-        
-        std::size_t foundEnd = buf.find("$");
-        length = buf.copy(local.longitude,foundEnd-found-1,found+1);
-        local.longitude[length]="\0";
-
-        cout<<"latitude: "<<local.latitude<<" longitude: "<<local.longitude<<endl;
-        return local;
-    }
+  
+		bytes_read =unistd::read(client, mes, sizeof(mes));	
+    	cout<<"read complete"<<endl;
+    	if( bytes_read > 0 ) {	
+			cout<<"bytes read = "<<bytes_read<<endl;
+			cout<<"mes: "<<mes<<endl;			
+			int t1=findLab(37);
+			copyToString((local.latitude),1,t1-1);  
+        	int t2=findLab(36);
+			copyToString((local.longitude),t1+1,t2-1);	     
+	        cout<<"latitude: "<<local.latitude<<" longitude: "<<local.longitude<<endl;
+		}
+		else{
+			cout<<"bytes_read:"<<bytes_read<<" error: "<<errno<<endl;
+			//	continue;
+		}
+		//}
+		return local;
 }
 
 int closeBluetooth(){
-    close(client);
-    close(s);
+    unistd::close(client);
+    unistd::close(s);
     return 0;
 }
+
 
 #endif /* bluetooth_h */
