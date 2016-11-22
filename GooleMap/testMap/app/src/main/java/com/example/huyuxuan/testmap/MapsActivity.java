de60de442e4bfdebd20c454f5b125255228dcc8b
sandy89876 <huyuxuan@huyuxuandeMacBook-Pro.local>
2016-11-22 23:46:50 +0800

    google map +藍牙app完成

---
+++ GooleMap/testMap/app/src/main/java/com/example/huyuxuan/testmap/MapsActivity.java
@@ -0,0 +1,319 @@
+package com.example.huyuxuan.testmap;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MapsActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener, OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    protected static final String TAG = "MainActivity";
+    private GoogleMap mMap;
+    protected GoogleApiClient mGoogleApiClient;
+    private TextView mLocationTextView;
+    private Location mLocation;
+    private View mapView;
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+
+    private BluetoothAdapter mAdapter;
+    private BluetoothSocket mSocket;
+    private BluetoothDevice mDevice;
+    private String address;
+    private OutputStream mOutStream;
+
+    private int curState = 0;//現在的連線狀態
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    private static final int NO_CONNECT = 0;
+    private static final int CONNECT = 1;
+
+    private Button testbut;
+    private Button locaBtn;
+    SupportMapFragment mapFragment;
+    mapThread myMapThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        testbut = (Button) findViewById(R.id.butTest);
+        locaBtn = (Button) findViewById(R.id.setPosiBtn);
+        testbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmp = "123.22%125.34$";
+                sendMes(tmp);
+            }
+        });
+        curState = NO_CONNECT;
+        mapFragment.getMapAsync(MapsActivity.this);
+        myMapThread = new mapThread();
+        myMapThread.start();
+        locaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                if (mLocation != null) {
+                    mLocationTextView.setText("My position=lat/lng:\n" +
+                            "(" + mLocation.getLatitude() +
+                            ", " + mLocation.getLongitude() + ")");
+                    String buffer = "1"+mLocation.getLatitude()+"%"+mLocation.getLongitude()+"$";
+                    sendMes(buffer);
+
+
+                } else {
+                    Toast.makeText(MapsActivity.this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+                }
+                mLocation = null;
+            }
+        });
+        if (mAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.finish();
+        } else {
+            if (!mAdapter.isEnabled()) {
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+                // Otherwise, setup the chat session
+            } else {
+                setBluetoothConnect();
+            }
+        }
+
+
+    }
+    private class mapThread extends Thread {
+        public void run(){
+
+            runOnUiThread(new Runnable() {
+                public void run()
+                {
+                    mapView = mapFragment.getView();
+                }
+            });
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
+        }
+
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    public void onConnected(Bundle connectionHint) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mLocationTextView = (TextView) findViewById(R.id.location_text);
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+
+        if (mapView != null &&
+                mapView.findViewById(Integer.parseInt("1")) != null) {
+            // Get the button view
+            View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+            // and next place it, on bottom right (as Google Maps app)
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationButton.getLayoutParams();
+            // position on right bottom
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 30);
+        }
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    setBluetoothConnect();
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving,
+                            Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+                break;
+        }
+    }
+
+    public void setBluetoothConnect(){
+        Intent serverIntent = new Intent(MapsActivity.this, DeviceListActivity.class);
+        startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+    }
+
+    public void connectDevice(Intent data){
+        address = data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        mDevice = mAdapter.getRemoteDevice(address);
+
+        BluetoothSocket tmp = null;
+        try{
+            //下面這兩個HTC手機有BUG
+            //tmp = mDevice.createRfcommSocketToServiceRecord(MY_UUID);
+            //  Method m = mDevice.getClass().getMethod("createRfcommSocket", new Class[] { int.class });
+
+            Method m = mDevice.getClass().getMethod("createInsecureRfcommSocket", new Class[] { int.class });
+            mSocket = (BluetoothSocket) m.invoke(mDevice, Integer.valueOf(22)); // 22==RFCOMM channel code
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        mAdapter.cancelDiscovery();
+        try{
+            mSocket.connect();
+            synchronized (this){
+                if(curState==CONNECT){
+                    return;
+                }
+                curState=CONNECT;
+            }
+        } catch(IOException e){
+            try{
+                mSocket.close();
+            } catch(IOException e1){
+                Log.d("bluetooth","unable to close socket");
+            }
+
+            curState=NO_CONNECT;
+        }
+        try {
+            mOutStream = mSocket.getOutputStream();
+        }catch(IOException e){
+            Log.d("bluetooth","getOutputStream has exception");
+        }
+
+    }
+
+    public void sendMes(String mes){
+            byte tmp[]= mes.getBytes();
+            try{
+                mOutStream.write(tmp);
+            }catch(IOException e){
+                Log.e(TAG, "Exception during write", e);
+            }
+            Toast.makeText(this,"send access"+mes, Toast.LENGTH_LONG).show();
+
+    }
+}
