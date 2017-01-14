907b5e698e8702d041d0076e5ccf4b0e16a60f67
s91503486f <s91503486f@yahoo.com.tw>
2017-01-15 00:16:20 +0800

    車子架構完成

--- LoRaTran/UnitTest/postman_car.cpp
+++ LoRaTran/UnitTest/postman_car.cpp
@@ -79,6 +79,7 @@ int main(){
 	int error;	//
 	double *longitude = NULL;
 	double *latitude = NULL;
+	string *packetKey = NULL;
 	
 	
 	error = setup(1);//
@@ -100,27 +101,46 @@ int main(){
 	*/
 	
 	
-	state = -1;
+	init(longitude,latitude,packetKey);
 	
 	//開始送貨循環
 	
 	cout << "Begin transport" << endl;
 	
-	//state -1->0 = idle - >接收到經緯度,開始送貨
+	//state -1->0 = idle - >接收到經緯度,開始到sender地點
 	recvSenderRequest();
 	
 	
-	//state 0->1 = 送貨->到達指定地點
+	//state 0->1 = 行走->到達sender指定地點
 	moveToSender();
 	
+	
+	//state 1->2 = 抵達->sender放入文件，開始前往recv點
+	beginTransport();
+	
+	
+	//state 2->3 = 抵達->告知抵達，並且接收packetKey
+	moveToReceiver();
+	
+	
+	//state 3->4 = 等待領或->輸入密碼成功，取貨完畢
+	endTransport();
 	
 	
     return 0;
 }
 
+void init(double *longitude,double *latitude,string *packetKey){
+	longitude = new double;
+	latitude = new double;
+	packetKey = new string;
+	state = -1;
+}
+
 int recvSenderRequest(double *longitude,double *latitude){
 	int StateCode;
 	
+	//確認state是否正確
 	StateCode = checkState(-1);
 	if(StateCode != CAR_OK){
 		cout << "recvSenderRequest : retrun error in method checkState before recv data" << endl;
@@ -149,6 +169,7 @@ int recvSenderRequest(double *longitude,double *latitude){
 	cout << "recvSenderRequest : recv longitude = " << *longitude << ", latitude = " << *latitude << endl;
 	
 	
+	//確認state是否正確
 	StateCode = checkState(-1);
 	if(StateCode != CAR_OK){
 		cout << "recvSenderRequest : retrun error in method checkState after recv data" << endl;
@@ -158,7 +179,7 @@ int recvSenderRequest(double *longitude,double *latitude){
 	state = 0;
 	
 	/*task
-		send 新state資訊給gateway
+		send 新state資訊給gateway(被動)
 	*/
 	
 	return CAR_OK;
@@ -171,6 +192,7 @@ int moveToSender(double dLon,double dLat){
 	bool isCarReach;	//車子是否到達
 	int StateCode;
 	
+	//確認state是否正確
 	StateCode = checkState(0);
 	if(StateCode != CAR_OK){
 		cout << "moveToSender : retrun error in method checkState before reach destnation" << endl;
@@ -195,6 +217,7 @@ int moveToSender(double dLon,double dLat){
 	} while ( -180 < sLon && sLon < 180);
 	
 	
+	//確認state是否正確
 	StateCode = checkState(0);
 	if(StateCode != CAR_OK){
 		cout << "moveToSender : retrun error in method checkState after reach destnation" << endl;
@@ -203,11 +226,173 @@ int moveToSender(double dLon,double dLat){
 	state = 1;
 	
 	/*task
-		send 新state資訊給gateway
+		send 新state資訊給gateway(主動)
 	*/
 	
 	return CAR_OK;
 }
+
+int beginTransport(double *longitude,double *latitude){
+	int StateCode;
+	
+	//確認state是否正確
+	StateCode = checkState(1);
+	if(StateCode != CAR_OK){
+		cout << "beginTransport : retrun error in method checkState before recv data" << endl;
+		return StateCode;
+	}
+	
+	
+	cout<<"beginTransport : Wait for Receiver location..."<<endl;
+	e = sx1272.receivePacketTimeout(10000);
+	
+	if(e != 0){
+		cout << "beginTransport : error! receive data error occur" << endl;
+		return e;
+	}
+	
+	for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
+    {
+        recv_packet[i] = (char)sx1272.packet_received.data[i];
+    }
+	
+	/*task
+		剖析楷甯給的字串變成資料
+		存到longitude與latitude兩個pointer裡面
+	*/
+	
+	
+	//判定使用者放入文件，目前使用enter做為判定
+	cin.getline();
+	
+	cout << "beginTransport : recv longitude = " << *longitude << ", latitude = " << *latitude << endl;
+	
+	
+	//確認state是否正確
+	StateCode = checkState(1);
+	if(StateCode != CAR_OK){
+		cout << "recvSenderRequest : retrun error in method checkState after recv data" << endl;
+		return StateCode;
+	}
+	
+	state = 2;
+	
+	/*task
+		send 新state資訊給gateway(主動)
+	*/
+}
+
+int moveToReceiver(double dLon,double dLat,string *packetKey){
+	double directionInfo, distanceInfo;	//方位與距離之回傳
+	double reachDistance = 0.01;	//判定多少距離內算到達(單位公里)
+	double sLon, sLat;	//起始地點
+	bool isCarReach;	//車子是否到達
+	int StateCode;
+	
+	//確認state是否正確
+	StateCode = checkState(2);
+	if(StateCode != CAR_OK){
+		cout << "moveToReceiver : retrun error in method checkState before reach destnation" << endl;
+		return StateCode;
+	}
+	
+	do {
+		
+		/*
+			透過GPS函式取得GPS資訊
+		*/
+		
+		isCarReach = isCarReachDestination(directionInfo, distanceInfo, reachDistance, sLon, sLat, dLon, dLat);
+		
+		if (isCarReach) {
+			cout << "moveToReceiver : reach destnation!" << endl;
+			break;
+		}
+		
+		cout << "You should still go toward "<< directionInfo << " degree for " << distanceInfo << " kilometer." << endl;
+		usleep(1000);
+	} while ( -180 < sLon && sLon < 180);
+	
+	//recv一組密碼
+	cout<<"moveToReceiver : Wait for PacketKey..."<<endl;
+	e = sx1272.receivePacketTimeout(10000);
+	
+	if(e != 0){
+		cout << "moveToReceiver : error! receive data error occur" << endl;
+		return e;
+	}
+	
+	for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
+    {
+        recv_packet[i] = (char)sx1272.packet_received.data[i];
+    }
+	
+	/*task
+		將recv的密碼分析成字串
+		存入packetKey內
+	*/
+	//目前先用9487來代替
+	*packetKey = "9487";
+	
+	
+	//確認state是否正確
+	StateCode = checkState(2);
+	if(StateCode != CAR_OK){
+		cout << "moveToReceiver : retrun error in method checkState after reach destnation" << endl;
+		return StateCode;
+	}
+	state = 3;
+	
+	/*task
+		send 新state資訊給gateway(主動)
+	*/
+	
+	return CAR_OK;
+}
+
+int endTransport(string *packetKey){
+	string input;
+	bool isCorrect = false;
+	
+	//確認state是否正確
+	StateCode = checkState(3);
+	if(StateCode != CAR_OK){
+		cout << "endTransport : retrun error in method checkState after reach destnation" << endl;
+		return StateCode;
+	}
+	
+	
+	cout << "endTransport : Wait for password input" << endl << endl;
+	do{
+		cout << "Password :";
+		cin.get(input,4);
+		
+		if(input != *packetKey){
+			cout << "Error! Please enter password again" << endl;
+		}
+		else{isCorrect = true;}
+	}while(!isCorrect);
+	
+	cout << "Password correct!" << endl;
+	cout << "Press enter to end transport!" << endl;
+	
+	/*
+		密碼箱打開後
+		要判定東西被拿走
+		這裡用按下enter來當作拿了
+	*/
+	cin.getline();
+	
+	
+	//確認state是否正確
+	StateCode = checkState(3);
+	if(StateCode != CAR_OK){
+		cout << "endTransport : retrun error in method checkState after reach destnation" << endl;
+		return StateCode;
+	}
+	state = 4;
+}
+
 
 int checkState(int tarState){
 	int returnCode;
