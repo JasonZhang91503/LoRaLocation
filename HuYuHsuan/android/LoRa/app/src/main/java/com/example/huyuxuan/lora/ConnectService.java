2e6b70957986c96263cac57c3c35a8fff403240c
sandy89876 <sandy89876@gmail.com>
2017-03-13 22:19:27 +0800

    登入debug

--- HuYuHsuan/android/LoRa/app/src/main/java/com/example/huyuxuan/lora/ConnectService.java
+++ HuYuHsuan/android/LoRa/app/src/main/java/com/example/huyuxuan/lora/ConnectService.java
@@ -2,6 +2,7 @@ package com.example.huyuxuan.lora;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -11,7 +12,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 
 /**
  * Created by huyuxuan on 2017/3/11.
@@ -20,53 +24,84 @@ import java.net.Socket;
 public class ConnectService extends IntentService {
 
     Socket mSocket;
+    InetAddress serverAddr;
+    SocketAddress sc_add;
     BufferedReader in;
     BufferedWriter out;
-    String type;
-    int id;
 
+    String type;
+    String id;
+    String account;
+    String password;
+    String name;
+    String email;
+    String msg;
+
+    String rcvMessage;
+    Boolean flag = false;
+
+    private static final String ACTION_RECV_MSG = "com.example.huyuxuan.lora.intent.action.RECEIVE_MESSAGE";
 
     public ConnectService(){
         super("ConnectService");
     }
 
+    @Override
     public void onCreate(){
-        try {
-            mSocket = new Socket(getString(R.string.ip),R.string.port);
-            if (mSocket.isConnected()) Log.i("Service", "Socket Connected");
-            in = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(mSocket.getOutputStream(),"utf8"));
-            Log.i("Service","BufferedReader and PrintWriter ready.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        super.onCreate();
+        Log.i("ConnectService:","onCreate");
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    serverAddr = InetAddress.getByName(getString(R.string.ip));
+                    mSocket = new Socket();
+                    sc_add = new InetSocketAddress(serverAddr,Integer.parseInt(getString(R.string.port)));
+                    if (mSocket.isConnected()){
+                        Log.i("Service", "Socket Connected");
+                    }else{
+                        mSocket.connect(sc_add,2000);
+                    }
+                    in = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+                    out = new BufferedWriter(new OutputStreamWriter(mSocket.getOutputStream(),"utf8"));
+                    Log.i("Service","BufferedReader and PrintWriter ready.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
 
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("Service:","onStartCommand called");
+
+        type = intent.getExtras().getString("type");
+        id = intent.getExtras().getString("id");
+
+        return START_NOT_STICKY;
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        type = intent.getExtras().getString("type");
-        id = intent.getExtras().getInt("id");
-        String msg = null;
-        if(type == "out"){
-            Log.i("Service", "type = out");
-            switch (id){
-                case 2://註冊
-                    String account = intent.getExtras().getString("account");
-                    String password = intent.getExtras().getString("password");
-                    String name = intent.getExtras().getString("name");
-                    String email = intent.getExtras().getString("email");
-                    msg = String.valueOf(id)+account+","+password+","+name+","+email+",";
+        Log.d("Service:","onHandleIntent called");
+
+        if(type.equals("out")){
+            Log.i("Service", "type = "+type+" id = "+id);
+
+            switch (id) {
+                case "2"://註冊
+                    account = intent.getStringExtra("account");
+                    password = intent.getStringExtra("password");
+                    name = intent.getStringExtra("name");
+                    email = intent.getStringExtra("email");
+                    msg = String.valueOf(id) + account + "," + password + "," + name + "," + email + ",";
                     break;
-                case 3://登入
-                    account = intent.getExtras().getString("account");
-                    password = intent.getExtras().getString("password");
-                    msg = String.valueOf(id)+account+","+password+",";
+                case "3"://登入
+                    account = intent.getStringExtra("account");
+                    password = intent.getStringExtra("password");
+                    msg = String.valueOf(id) + account + "," + password + ",";
                     break;
             }
             if (!mSocket.isOutputShutdown() && msg.length() > 0){
@@ -74,16 +109,28 @@ public class ConnectService extends IntentService {
                     out.write(msg);
                     out.flush();
                     Log.d("Service","write "+msg+" to server");
+                    rcvMessage = in.readLine();
+                    Log.d("Service","receive "+rcvMessage+" from server");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }else if(type == "in"){
             //要接收
+            try {
+                rcvMessage = in.readLine();
+                Log.d("Service","receive "+rcvMessage+" from server");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(ACTION_RECV_MSG);
+        broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+        broadcastIntent.putExtra("result", flag.toString());
+        sendBroadcast(broadcastIntent);
     }
-
-
 
 
 }
--- HuYuHsuan/android/LoRa/app/src/main/java/com/example/huyuxuan/lora/MainActivity.java
+++ HuYuHsuan/android/LoRa/app/src/main/java/com/example/huyuxuan/lora/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.huyuxuan.lora;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,37 +15,63 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnSignUp;
 
+    private ConnectServiceReceiver receiver;
+    private static final String ACTION_RECV_MSG = "com.example.huyuxuan.lora.intent.action.RECEIVE_MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogin = (Button)findViewById(R.id.btnLogin);
-        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ConnectService.class);
+                Intent intent = new Intent(MainActivity.this,ConnectService.class);
+
                 intent.putExtra("type","out");
-                intent.putExtra("id",3);
+                intent.putExtra("id","3");
                 intent.putExtra("account","dfdf");
                 intent.putExtra("password","1123");
+
                 startService(intent);
+                Log.d("MainActivity:", "start ConnectService");
             }
         });
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ConnectService.class);
-                intent.putExtra("type","out");
-                intent.putExtra("id",2);
-                intent.putExtra("account","dfdf");
-                intent.putExtra("password","1123");
-                intent.putExtra("name","huhu");
-                intent.putExtra("email","ssdd@gmail.com");
+                Intent intent = new Intent(ConnectService.class.getName());
+                intent.putExtra("type", "out");
+                intent.putExtra("id", "2");
+                intent.putExtra("account", "dfdf");
+                intent.putExtra("password", "1123");
+                intent.putExtra("name", "huhu");
+                intent.putExtra("email", "ssdd@gmail.com");
                 startService(intent);
             }
         });
+
+        //动态注册receiver
+        IntentFilter filter = new IntentFilter(ACTION_RECV_MSG);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        receiver = new ConnectServiceReceiver();
+        registerReceiver(receiver, filter);
     }
 
+    //接收广播类
+    public class ConnectServiceReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("result");
+            Log.d("ConnectServiceReceiver", message);
+
+            // 如果傳回成功
+            if(message.equals("true")){
+
+            }
+
+        }
+    }
 }
