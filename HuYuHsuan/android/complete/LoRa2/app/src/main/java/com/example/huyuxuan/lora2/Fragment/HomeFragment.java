9c16cf2387ad2240acc499309f88172c4d8f98ef
sandy89876 <sandy89876@gmail.com>
2017-05-03 22:20:26 +0800

    debug

--- HuYuHsuan/android/complete/LoRa2/app/src/main/java/com/example/huyuxuan/lora2/Fragment/HomeFragment.java
+++ HuYuHsuan/android/complete/LoRa2/app/src/main/java/com/example/huyuxuan/lora2/Fragment/HomeFragment.java
@@ -1,5 +1,13 @@
 package com.example.huyuxuan.lora2.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -13,17 +21,33 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.huyuxuan.lora2.ConnectService;
 import com.example.huyuxuan.lora2.R;
+import com.example.huyuxuan.lora2.RoundImageView;
+import com.example.huyuxuan.lora2.SelectDialog;
+import com.example.huyuxuan.lora2.SelectDialog.SelectDialogListener;
+import com.example.huyuxuan.lora2.UIUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
 
 /**
  * Created by huyuxuan on 2017/4/28.
@@ -41,6 +65,13 @@ public class HomeFragment extends Fragment {
     private ConnectServiceReceiver receiver;
     private static final String ACTION_RECV_MSG = "com.example.huyuxuan.lora.intent.action.RECEIVE_MESSAGE";
     static java.text.SimpleDateFormat dayDateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+    final private int MY_REQUEST_CODE = 123;
+
+    private RoundImageView mHeadImage;
+    private final int PHOTO_PICKED_FROM_CAMERA = 1; // 用来标识头像来自系统拍照
+    private final int PHOTO_PICKED_FROM_FILE = 2; // 用来标识从相册获取头像
+    private final int CROP_FROM_CAMERA = 3;
+    private Uri imgUri; // 由于android手机的图片基本都会很大，所以建议用Uri，而不用Bitmap
 
 
     @Override
@@ -54,11 +85,17 @@ public class HomeFragment extends Fragment {
         //inflate layout for this fragment
         myview = inflater.inflate(R.layout.fragment_home,container,false);
         lv = (ListView)myview.findViewById(R.id.home_listview);
-
         btnProfile = (Button)myview.findViewById(R.id.btnProfile);
         btnRegister = (Button)myview.findViewById(R.id.btnHomeToRegister);
+        mHeadImage = (RoundImageView)myview.findViewById(R.id.main_roundImage);
         isBind = false;
         updateListView();
+
+        String sd = Environment.getExternalStorageDirectory().toString();
+        Bitmap bitmap = BitmapFactory.decodeFile(sd + "/mypic.png");
+        if(bitmap != null){
+            mHeadImage.setImageBitmap(bitmap);
+        }
 
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +119,56 @@ public class HomeFragment extends Fragment {
                         .replace(R.id.fragment_container,registerFragment).commit();
             }
         });
+        mHeadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<String> list = new ArrayList<>();
+                list.add("拍照");
+                list.add("相册");
+                showDialog(new SelectDialogListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        switch (position){
+                            case 0:
+                                //用系統相機拍照
+
+                                if (checkSelfPermission(getActivity().getApplicationContext(),Manifest.permission.CAMERA)
+                                        != PackageManager.PERMISSION_GRANTED) {
+
+                                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                                            MY_REQUEST_CODE);
+                                }else{
+                                    openCamera();
+                                }
+
+                                Log.d("HomeFragment","PHOTO-PICK-FROM-CAMERA");
+                                break;
+                            case 1:
+                                //開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因
+                                //為點選相片後返回程式呼叫onActivityResult
+                                Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                intent.setType("image/*");
+                                startActivityForResult(intent, PHOTO_PICKED_FROM_FILE);
+                                Log.d("HomeFragment","PHOTO-PICK-FROM-FILE");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                },list);
+            }
+        });
+
         return myview;
+    }
+
+    private SelectDialog showDialog(SelectDialogListener listener, List<String> list){
+        Log.d("HomeFragment","showDialog");
+        //getActivity?
+        SelectDialog dialog = new SelectDialog(getActivity(),
+                R.style.transparentFrameWindowStyle,listener,list);
+        dialog.show();
+        return dialog;
     }
 
     public void updateListView(){
@@ -168,4 +254,126 @@ public class HomeFragment extends Fragment {
         super.onPause();
         Log.d("HomeFragment","onPause");
     }
+
+    //拍照完畢或選取圖片後呼叫此函式
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,Intent data)
+    {
+        Log.d("HomeFragment","onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("HomeFragment","resultCode="+resultCode+"requestCode="+requestCode);
+        if (resultCode != RESULT_OK){
+            return;
+        }
+        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
+        switch (requestCode) {
+            case PHOTO_PICKED_FROM_CAMERA:
+                doCrop();
+                break;
+            case PHOTO_PICKED_FROM_FILE:
+                imgUri = data.getData();
+                doCrop();
+                break;
+            case CROP_FROM_CAMERA:
+                if (data != null){
+                    setCropImg(data);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    //裁剪圖片
+    private void doCrop(){
+        Log.d("HomeFragment","doCrop");
+        final Intent intent = new Intent("com.android.camera.action.CROP");
+        //可以选择图片类型，如果是*表明所有类型的图片
+        intent.setDataAndType(imgUri, "image/*");
+        // 下面这个crop = true是设置在开启的Intent中设置显示的VIEW可裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例，这里设置的是正方形（长宽比为1:1）
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 300);
+        intent.putExtra("outputY", 300);
+        //裁剪时是否保留图片的比例，这里的比例是1:1
+        intent.putExtra("scale", true);
+        //是否是圆形裁剪区域，设置了也不一定有效
+        //intent.putExtra("circleCrop", true);
+        //设置输出的格式
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        //是否将数据保留在Bitmap中返回
+        intent.putExtra("return-data", true);
+
+        startActivityForResult(intent,CROP_FROM_CAMERA);
+
+
+    }
+    private void setCropImg(Intent picData){
+        Log.d("HomeFragment","setCropImg");
+        Bundle bundle = picData.getExtras();
+        if (bundle != null){
+            Bitmap mBitmap = bundle.getParcelable("data");
+            mHeadImage.setImageBitmap(mBitmap);
+            saveBitmap(Environment.getExternalStorageDirectory() + "/mypic.png",mBitmap);
+        }
+    }
+    private void saveBitmap(String fileName,Bitmap bitmap){
+        Log.d("HomeFragment","saveBitmap");
+        if (checkSelfPermission(getActivity().getApplicationContext(),Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_REQUEST_CODE);
+        }
+        File file = new File(fileName);
+        FileOutputStream fout = null;
+        try {
+            file.createNewFile();
+            fout = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG,100,fout);
+            fout.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fout!=null){
+                    fout.close();
+                }
+                UIUtil.showToast(getActivity(),"保存成功！");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Now user should be able to use camera
+                openCamera();
+            }
+            else {
+                // Your app will not have this permission. Turn off all functions
+                // that require this permission or it will force close like your
+                // original question
+                Toast.makeText(getActivity(),"請開啟相機權限", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    public void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imgUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+                "avatar_"+String.valueOf(System.currentTimeMillis())+".png"));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imgUri);
+        startActivityForResult(intent,PHOTO_PICKED_FROM_CAMERA);
+        Log.d("HomeFragment","openCamera");
+    }
+
 }
--- HuYuHsuan/android/complete/LoRa2/app/src/main/java/com/example/huyuxuan/lora2/Fragment/ProfileFragment.java
+++ HuYuHsuan/android/complete/LoRa2/app/src/main/java/com/example/huyuxuan/lora2/Fragment/ProfileFragment.java
@@ -1,14 +1,27 @@
 package com.example.huyuxuan.lora2.Fragment;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.huyuxuan.lora2.R;
+
+import java.io.FileNotFoundException;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -22,7 +35,10 @@ public class ProfileFragment extends Fragment{
     private TextView myAccountView;
     private TextView myEmailView;
 
+
     private SharedPreferences sharedPreferences;
+
+
 
     @Override
     public void onCreate(Bundle savedInstances){
@@ -36,12 +52,17 @@ public class ProfileFragment extends Fragment{
         myNameView = (TextView)myview.findViewById(R.id.tvProfileName);
         myAccountView = (TextView)myview.findViewById(R.id.tvProfileAccount);
         myEmailView = (TextView)myview.findViewById(R.id.tvProfileEmail);
+
         sharedPreferences = getActivity().getSharedPreferences("data" , MODE_PRIVATE);
 
         myNameView.setText(sharedPreferences.getString(getString(R.string.name),""));
         myAccountView.setText(sharedPreferences.getString(getString(R.string.account),""));
         myEmailView.setText(sharedPreferences.getString(getString(R.string.email),""));
 
+
+
         return myview;
     }
+
+
 }
