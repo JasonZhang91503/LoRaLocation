2c30a4870f47a9ab194393fd287b691217fe841f
s91503486f <s91503486f@yahoo.com.tw>
2017-05-27 23:30:47 +0800

    test

--- LoRaTran/carControl.cpp
+++ LoRaTran/carControl.cpp
@@ -1,8 +1,35 @@
+#include <stdio.h>  
+#include <termios.h>  
+#include <unistd.h>  
+#include <fcntl.h>  
+
 #include"rs232.h"
 #include<iostream>
 using namespace std;
 
 #define TTYUSB0 16
+
+int kbhit(void)  
+{  
+    struct termios oldt, newt;  
+    int ch;  
+    int oldf;  
+    tcgetattr(STDIN_FILENO, &oldt);  
+    newt = oldt;  
+    newt.c_lflag &= ~(ICANON | ECHO);  
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);  
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);  
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);  
+    ch = getchar();  
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  
+    fcntl(STDIN_FILENO, F_SETFL, oldf);  
+    if(ch != EOF)  
+    {  
+        ungetc(ch, stdin);  
+        return 1;  
+    }  
+    return 0;  
+}  
 
 
 int main(){
@@ -19,30 +46,36 @@ int main(){
     goX[15] = {"FE FE XX 00 FF"},
     goY[15] = {"FE FE YY 00 FF"};
 
-    int input;
-    cout << "Input :";
-    cin >> input;
 
-    switch(input){
-    case 1:
-        RS232_SendBuf(TTYUSB0,power,11);
-        break;
-    case 2:
-        RS232_SendBuf(TTYUSB0,speedHigh,11);
-        break;
-    case 3:
-        RS232_SendBuf(TTYUSB0,speedLow,11);
-        break;
-    case 4:
-        RS232_SendBuf(TTYUSB0,goX,14);
-        break;
-    case 5:
-        RS232_SendBuf(TTYUSB0,goY,14);
-        break;
-    }
+    do{
 
-    
-    
+        while(!kbhit());
 
-    
+        char c = getchar();
+
+        /*
+        int input;
+        cout << "Input :";
+        cin >> input;
+        */
+        switch(c){
+        case 'e':
+            RS232_SendBuf(TTYUSB0,power,11);
+            break;
+        case 'h':
+            RS232_SendBuf(TTYUSB0,speedHigh,11);
+            break;
+        case 'l':
+            RS232_SendBuf(TTYUSB0,speedLow,11);
+            break;
+        case 'x':
+            RS232_SendBuf(TTYUSB0,goX,14);
+            break;
+        case 'y':
+            RS232_SendBuf(TTYUSB0,goY,14);
+            break;
+        }
+
+        
+    }while(c != 'q');
 }
