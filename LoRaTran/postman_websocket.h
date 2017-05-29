096e259494c0a4ad6021e122f387f9e69d5ed8a7
s91503486f <s91503486f@yahoo.com.tw>
2017-05-29 15:13:40 +0800

    測試

--- LoRaTran/postman_websocket.h
+++ LoRaTran/postman_websocket.h
@@ -14,10 +14,12 @@ typedef websocketpp::server<websocketpp::config::asio> server;
 #ifndef NO_CAR_MODE
 void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg)
 {
-  	std::cout << msg->get_payload() << std::endl;
+    std::cout << msg->get_payload() << std::endl;
 }
 
 void* asyncWebSocketServer(void* param){
+    cout << "asyncWebSocketServer start\n";
+    /*
     server print_server;
 
     print_server.set_message_handler(&on_message);
@@ -27,6 +29,43 @@ void* asyncWebSocketServer(void* param){
     print_server.start_accept();
 
     print_server.run();
+    */
+    utility_server s;
+    s.run();
+
 }
+
+class utility_server {
+public:
+    utility_server() {
+        m_endpoint.set_message_handler(&echo_handler);
+
+        // Initialize Asio
+        m_endpoint.init_asio();
+    }
+    
+
+    void echo_handler(websocketpp::connection_hdl hdl, server::message_ptr msg) {
+        // write a new message
+        std::cout << msg->get_payload() << std::endl;
+        m_endpoint.send(hdl, msg->get_payload(), msg->get_opcode());
+    }
+
+    void run() {
+        // Listen on port 9002
+        m_endpoint.listen(9002);
+
+        // Queues a connection accept operation
+        m_endpoint.start_accept();
+
+        // Start the Asio io_service run loop
+        m_endpoint.run();
+    }
+private:
+    server m_endpoint;
+};
+
+
+
 #endif
 
